f64e79d8e1440523f55c5fdd62f7ef36b0b52fec
Wellington <wgn1910ww@gmail.com>
2026-05-05 21:43:38 -0300

    Ficha medica finalizada

--- Ficha_medica.py
+++ Ficha_medica.py
@@ -1,75 +1,161 @@
-while True: 
+from datetime import datetime
 
-    print('Ficha médica')
-
-    print('Preencha todos os dados a seguir')
-
-    nome = input('Digite seu nome completo: ')
-    data_nascimento = input('Digite sua data de nascimento:  ')
-    endereço = input('Digite seu endereço: ')
-    cartão_sus = input('Digite seu cartão do SUS: ')
-    nume_tel = input('Numero para contato: ')
-    nome_mãe = input('Nome da mãe: ')
+def ficha_cadastro():
+    nome = input('Digite seu nome completo: ') 
+    nome_mae = input('Nome da Mãe: ')
     nome_pai = input('Nome do pai: ')
-    tipo_sanguineo = input('Tipo sanguineo: ')
-    fumante = input('Fumante: ')
-    alergia_med = input ('Possui alergia a medicamento: ')
+       
+    while True:
+        try: 
+            idade = int(input('Digite sua idade: '))
+            if idade >= 1 and idade < 120:
+                break
+        except ValueError:
+            print('Você não digitou um numero, tente novamente')
+    
+    moradia_local = endereco()
+    telefone = tel()
+    cartao_sus = sus()
+    data = data_nasc()
+    tipo_sangue = sangue()
+    fum = fumante()
+    aler = alergia()
+
+    cadastro = {
+        'nome_completo': nome,
+        'data_nasc': data, 
+        'anos': idade,
+        'mae': nome_mae,
+        'pai': nome_pai,
+        'local': moradia_local,
+        'numero_tel': telefone,
+        'numero_sus': cartao_sus,
+        'sanguineo': tipo_sangue,
+        'fuma': fum,
+        'alergico': aler 
+
+    }
+
+    return cadastro
+
+def endereco():
+    while True:
+        moradia_paciente = input('Digite seu endereço completo: ').strip()
+        if not moradia_paciente:
+            print('Campo obrigatorio, tente novamente')
+            continue
+        return moradia_paciente
+
+def tel():
+    while True: 
+        telefone = input('Digite seu numero de telefone : ')  
+        if telefone.isdigit() and len(telefone) ==  11:
+            return telefone
+        else:
+            print('Numero de telefone é invalido, tente novamente')
+    
+def sus():
+    while True:
+        cartao_sus = input('Digite o número do cartão do sus(contendo os 15 números): ')
+    
+        if cartao_sus.isdigit() and len(cartao_sus) == 15:
+            return cartao_sus    
+        else:
+            print('Confira se o adicionou os 15 numeros corretos, tente novamente')
+    
+def data_nasc():
+    while True:
+        nasc = input('Digite a sua data de nascimento (DDMMAAAA): ')
+        if not nasc.isdigit():
+            print('Digite apenas numeros')
+            continue
+
+        if len(nasc) != 8:
+            print('Digite a data no formato DD/MM/AAAA')
+        try:
+            data = datetime.strptime(nasc, "%d%m%Y")
+            return data
+            
+        except ValueError:
+            print('Data invalida')
+            continue
+
+def sangue():
+    while True:
+        tipo_sangue = input('Digite seu tipo sanguineo(A+, A-, B+, B-, AB+, AB-, O+ e O-): ').upper()
+        if tipo_sangue not in ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-']:
+            print('Tipo sanguineo invalido, tente novamente')
+            continue
+        return tipo_sangue
+    
+def fumante():
+    while True:
+        usuario_fumante = input('Paciente é fumante ou não? (Não ou Sim): ').lower()
+        if usuario_fumante not in ['sim', 'não', 'nao']:
+            print('Você não digitou corretamente, tente novamente')
+            continue
+        return usuario_fumante    
+
+def alergia():
+    while True:
+        usuario_alergico = input('Paciente é alergico a alguma médicação: ').upper()
+        if usuario_alergico not in ['NÃO', 'SIM']:
+            print('Você não digitou corretamente, tente novamente')
+            continue
+        return usuario_alergico
+
+lista_pacientes = []
+
+while True:
+    print('MENU')
+    print()
+    print('Digite as opções a seguir: ')
+    print('Digite 1, para adicionar paciente')
+    print('Digite 2, para listar paciente')
+    print('Digite 3, para remover algum paciente da lista')
+    print('Digite 4, para sair da lista')
+    print()
 
 
-    if nome.replace(' ', '').isalpha():
-        print('Voce digitado corretamente')
-    else:
-        print('Digite apenas letras (nome)')
+
+    try:
+        usuario = int(input('Digite a opção desejada (de 1 a 4): '))
+        
+    except ValueError:
+        print('Digite apenas numeros, tenta novamente')
+        continue
+
+    if usuario == 1:
+        print('Você digitou -> 1 <- para adicionar paciente')
+        paciente = ficha_cadastro()
+        lista_pacientes.append(paciente)
+        print('Paciente adicionado com sucesso')
+    
+    elif usuario == 2:
+        if lista_pacientes == []:
+            print('Sua lista se encontra vazia')
+            continue
+        print('Selecionou -> 2 <-, Lista de paciente: ')
+
+        for i, paciente in enumerate(lista_pacientes, start=1):
+            print(f"{i} - {paciente['nome_completo']}")
 
 
-    if  data_nascimento.replace('/', '').isnumeric():
-        print('Data de nascimento digitada corretamente')
-    else:
-        print('Digite apenas numeros(data_nasc)')
-
-
-    if endereço.isalpha() and endereço.isnumeric:
-        print('Nome ou numero incorreto (endereço)')
-    else:
-        print('Endereço digitado corretamente')
-
-
-    if len(cartão_sus) < 14 or len(cartão_sus) >16  :
-        print('Numeros a mais ou a menos (numero SUS)')
-    else:
-        print('Numero do sus correto')
-
-
-    if len (nume_tel) < 8 or len(nume_tel) > 10:   
-        print('Numeros a mais ou a menos (Telefone)')
-    else:
-        print('Numero de telefone esta correto')
-
-
-    if nome_mãe.replace(' ', '').isalpha() and nome_pai.replace(' ', '').isalpha():
-        print('Digitado corretamente')
-    else:
-        print('Voce digitou algo de errado, olha novamente (Pais)')
-
-
-    if len(tipo_sanguineo) > 3 :
-        print('Tipo Sanguineo incorreto (Tipo de sangue)')
-    else:
-        print('Tipo sanguineo cadastrado')
-
-
-    if fumante.lower() in ['Sim', 'sim', 'nao', 'Não', 'não']:
-        print('Informação valida')
-    else:
-        print('Responda apenas (Sim ou Não)')
-
-    if alergia_med.lower() in ['Sim', 'sim', 'Não', 'não', 'nao']:
-        print('Informação valida')
-    else:
-        print('Responda (Sim ou Não)')
-
-    print('Cadastro realizado com sucesso')
-
-    sair = input('Deseja [s]air: ').lower().startswith('s')
-    if sair is True:
+    elif usuario == 3:
+        if lista_pacientes == []:
+            print('Sua lista se encontra vazia')
+            continue
+        remover_paci = int(input('Digite o numero do paciente: '))
+        
+        if 1 <= remover_paci <= len(lista_pacientes):
+            lista_pacientes.pop(remover_paci - 1)
+            print('Paciente removido com sucesso')
+        else:
+            print('Número inválido')
+        
+        for i, paciente in enumerate(lista_pacientes, start=1):
+           print(f"{i} - {paciente['nome_completo']}")
+    
+    elif usuario == 4:
+        print('ENCENRRANDO O PROGRAMA DE PACIENTE')
         break
